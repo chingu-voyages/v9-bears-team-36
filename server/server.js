a1980af709eb37204ea14ef5828e0fe0ca2cd77a
Marshall Danel <marshalldanel@gmail.com>
2019-06-06 06:49:23 -0700

    Add initial server setup

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || 'development';
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => {
+  res.status(200).send();
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT} in ${ENV} mode. Wait for compile...`)
+);

1b10005279b0ae167944dbff13f72541be36db8a
Marshall Danel <marshalldanel@gmail.com>
2019-06-08 15:11:10 -0700

    Add reliefWeb route, remove basic home route

--- server/server.js
+++ server/server.js
@@ -6,12 +6,12 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const ENV = process.env.NODE_ENV || 'development';
 
+const reliefWeb = require('./routes/reliefWeb');
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (_, res) => {
-  res.status(200).send();
-});
+app.use('/relief', reliefWeb);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'));

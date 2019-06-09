cdb8d4577e0aabdae285593e61e96dceb05ddb1b
Marshall Danel <marshalldanel@gmail.com>
2019-06-09 12:39:04 -0700

    Modify response data structure

--- server/routes/reliefWeb.js
+++ server/routes/reliefWeb.js
@@ -23,47 +23,23 @@ router.get('/', function(_, res) {
             description: fields.description,
             id: fields.id,
             name: fields.name,
-            primary_country: {
-              id: fields.primary_country.id,
-              location: {
-                lat: fields.primary_country.location.lat,
-                lon: fields.primary_country.location.lon
-              },
-              name: fields.primary_country.name,
-              type: fields.primary_type.name,
-              type_id: fields.primary_type.id
-            },
-            secondary_countries: fields.country
-              .filter(country => {
-                if (country.id === fields.primary_country.id) {
-                  return false;
-                }
-                return true;
-              })
-              .map(country => {
-                return {
-                  country: country.name,
-                  id: country.id,
-                  location: {
-                    lat: country.location.lat,
-                    lon: country.location.lon
-                  }
-                };
-              }),
-            secondary_types: fields.type
-              .filter(type => {
-                if (type.primary) {
-                  return false;
-                }
-                return true;
-              })
-              .map(type => {
-                return {
-                  id: type.id,
-                  type: type.name
-                };
-              }),
-            status: fields.status
+            countries: fields.country.map(country => {
+              return {
+                id: country.id,
+                location: {
+                  lat: country.location.lat,
+                  lon: country.location.lon
+                },
+                name: country.name,
+                primary: country.primary ? country.primary : false
+              };
+            }),
+            types: fields.type.map(type => {
+              return {
+                id: type.id,
+                type: type.name
+              };
+            })
           };
         });
 

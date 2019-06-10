11e8fa9ca267d06b04d635b5e6521a2dac2f4e85
mawarijo <mdopure@gmail.com>
2019-06-09 21:39:13 -0700

    search bar sorting and view all

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,13 +2,19 @@ import React from "react";
 
 import disasters from "../seedData";
 
-const SearchBar = ({ onChange, onSubmit, value }) => (
+const SearchBar = ({ onChange, onReset, onSubmit, value }) => (
   <div>
     <input onChange={onChange} placeholder="Find by Country" value={value} />
+    <button onClick={onReset}>View All</button>
     {value && (
       <div>
         {disasters
           .filter(disaster => disaster.name.toLowerCase().includes(value))
+          .sort(
+            (a, b) =>
+              a.name.toLowerCase().indexOf(value) >
+              b.name.toLowerCase().indexOf(value)
+          )
           .map(disaster => (
             <button key={disaster.id} onClick={onSubmit} value={disaster.id}>
               {disaster.name}

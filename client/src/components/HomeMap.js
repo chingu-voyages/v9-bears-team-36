4e79adf2438016874767412915d7a43b0e105a93
Marshall Danel <marshalldanel@gmail.com>
2019-06-23 10:39:35 -0700

    Add markers for all disasters

--- client/src/components/HomeMap.js
+++ client/src/components/HomeMap.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = [{ width: '100%' }, { height: '100%' }];
 
 class HomeMap extends Component {
   render() {
+    const { google, data } = this.props;
     return (
       <Map
-        google={this.props.google}
+        google={google}
         zoom={2}
         styles={mapStyles}
         initialCenter={{ lat: 41.2284, lng: 80.9098 }}
-      />
+      >
+        {data.map(location => {
+          const { lat } = location.countries[0].location;
+          const { lng } = location.countries[0].location;
+          return (
+            <Marker
+              name={location.name}
+              key={location.id}
+              position={{
+                lat,
+                lng
+              }}
+            />
+          );
+        })}
+      </Map>
     );
   }
 }

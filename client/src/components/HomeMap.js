219fa8eaadc1b2090dc71f054407f7df8d6144a3
Marshall Danel <marshalldanel@gmail.com>
2019-06-23 11:12:27 -0700

    Add initial marker infoWindow

--- client/src/components/HomeMap.js
+++ client/src/components/HomeMap.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 const mapStyles = [{ width: '100%' }, { height: '100%' }];
 
 class HomeMap extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  };
+
+  onMapClick = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     const { google, data } = this.props;
     return (
@@ -12,6 +35,7 @@ class HomeMap extends Component {
         zoom={2}
         styles={mapStyles}
         initialCenter={{ lat: 41.2284, lng: 80.9098 }}
+        onClick={this.onMapClick}
       >
         {data.map(location => {
           const { lat } = location.countries.filter(
@@ -22,6 +46,7 @@ class HomeMap extends Component {
           )[0].location;
           return (
             <Marker
+              onClick={this.onMarkerClick}
               name={location.name}
               key={location.id}
               position={{
@@ -31,6 +56,14 @@ class HomeMap extends Component {
             />
           );
         })}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <>
+            <a href="/specificDisaster">{this.state.selectedPlace.name}</a>
+          </>
+        </InfoWindow>
       </Map>
     );
   }

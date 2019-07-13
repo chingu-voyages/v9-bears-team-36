db567e90e016865665244138d883b5be5c79e408
mawarijo <mdopure@gmail.com>
2019-07-12 22:11:57 -0700

    further styles

--- client/src/components/ArticleCard.js
+++ client/src/components/ArticleCard.js
@@ -2,28 +2,34 @@ import React from 'react';
 
 import './ArticleCard.scss';
 
-const ArticleCard = ({ article }) => (
-  <a
-    className="card"
-    href={article.web_url}
-    rel="noopener noreferrer"
-    target="_blank"
-    title={article.headline.main}
-  >
-    {article.multimedia ? (
-      <div
-        className="card__image"
-        style={{ backgroundImage: `url('${article.multimedia}')` }}
-      />
-    ) : (
-      <div className="image__background" />
-    )}
-    <h3 className="card__title">{article.headline.main}</h3>
-    <p className="card__abstract">{article.abstract}</p>
-    <p className="card__read-time">
-      {Math.round(article.word_count / 250)} min read
-    </p>
-  </a>
-);
+const ArticleCard = ({ article }) => {
+  const ellipsis = article.abstract.split(' ').length > 26 && '...';
+  return (
+    <a
+      className="card"
+      href={article.web_url}
+      rel="noopener noreferrer"
+      target="_blank"
+      title={article.headline.main}
+    >
+      {article.multimedia ? (
+        <div
+          className="card__image"
+          style={{ backgroundImage: `url('${article.multimedia}')` }}
+        />
+      ) : (
+        <div className="image__background" />
+      )}
+      <h3 className="card__title">{article.headline.main}</h3>
+      <p className="card__abstract">
+        {article.abstract.split(' ', 26).join(' ')}
+        {ellipsis}
+      </p>
+      <p className="card__read-time">
+        {Math.round(article.word_count / 250)} min read
+      </p>
+    </a>
+  );
+};
 
 export default ArticleCard;
